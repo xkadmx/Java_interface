04c88b7921bafb9cdf3127d771e41f265d9d1e27
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-29 20:22:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/java_interface/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.java_interface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout zuzaslayout = new RelativeLayout(this);
+        Button redButton = new Button(this);
+
+        zuzaslayout.addView(redButton);
+        setContentView(zuzaslayout);
+    }
+}
